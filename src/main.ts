f33849f5a69fb35970d9d590ac5753d0fb64ef6f
KhangNguyen <nguyenduykhangcd12t4@gmail.com>
2021-10-11 20:21:08 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as session from 'express-session'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:true,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
