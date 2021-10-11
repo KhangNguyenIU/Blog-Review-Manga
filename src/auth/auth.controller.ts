0f650e685eb7988138af944146dad1a8871e3c19
KhangNguyen <nguyenduykhangcd12t4@gmail.com>
2021-10-11 19:03:22 +0700

    fix22

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -56,10 +56,11 @@ export class AuthController {
   }
 
   @Post('/test')
-  testSetCookie(@Res({passthrough:true}) res: Response) {
-     res.cookie('test-cooken', 'coooooooooookieeeeeeeeeeeeeee')
-      
-      return res.status(200).json({ message: 'set cookie' });
+  testSetCookie(@Res({ passthrough: true }) res: Response) {
+    return res
+      .cookie('test-cooken', 'coooooooooookieeeeeeeeeeeeeee')
+      .status(200)
+      .json({ message: 'set cookie' });
   }
 
   @Get('/test')
