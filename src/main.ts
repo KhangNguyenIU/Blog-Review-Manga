fce5b1f82d3e5d7a96bfaf71ae9f7e68aa2f5114
KhangNguyen <nguyenduykhangcd12t4@gmail.com>
2021-10-14 21:31:33 +0700

    fix29

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,12 @@ async function bootstrap() {
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   const port: number = parseInt(`${process.env.PORT}`) || 8000;
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
 
   await app.listen(port, () => {
     console.log('Server is running on port:  ', port);
