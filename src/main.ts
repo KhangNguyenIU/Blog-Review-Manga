c748d4f9c4577e5087ec352b5ed2400da6b08fa2
KhangNguyen <nguyenduykhangcd12t4@gmail.com>
2021-10-14 20:53:55 +0700

    fix26

--- src/main.ts
+++ src/main.ts
@@ -3,27 +3,11 @@ import { AppModule } from './app.module';
 
 import { urlencoded, json } from 'express';
 import * as cookieParser from 'cookie-parser';
-import * as config from 'config';
-import * as session from 'express-session';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import * as fs from 'fs'
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log('allowed cors for:', origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log('blocked cors for:', origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-  //   credentials: true,
-  // });
+
+  const app = await NestFactory.create(AppModule);
   app.enableCors()
   app.use(cookieParser());
 
@@ -31,6 +15,13 @@ async function bootstrap() {
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   const port: number = parseInt(`${process.env.PORT}`) || 8000;
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+  
   await app.listen(port, () => {
     console.log('Server is running on port:  ', port);
   });
